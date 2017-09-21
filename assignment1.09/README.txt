392a82880d1be62582faa9e18011178fca31a080
Devin1282 <dl.johnson.1995@gmail.com>
2017-09-20 23:03:22 -0500

    Add files via upload

---
+++ assignment1.09/README.txt
@@ -0,0 +1,9 @@
+README:
+
+Created functions in io.cpp to handle drawing the inventory and equipment list
+
+Modified combat function in move.cpp to calculate damage.
+
+Added function in do_moves function to pick up objects if walked over.
+
+Modified pc.h to hold inventory and equipment arrays
---
+++ assignment1.09/pc.h
@@ -0,0 +1,35 @@
+#ifndef PC_H
+# define PC_H
+
+# include <stdint.h>
+
+# include "dims.h"
+# include "character.h"
+# include "dungeon.h"
+
+class pc : public character {
+ public:
+  terrain_type_t known_terrain[DUNGEON_Y][DUNGEON_X];
+  unsigned char visible[DUNGEON_Y][DUNGEON_X];
+  object *inventory[10];
+  object *equipment[12];
+};
+
+class object;
+typedef struct dungeon dungeon_t;
+#include "dungeon.h"
+
+int get_empty_inventory_slot(dungeon_t *d);
+uint32_t pc_is_alive(dungeon_t *d);
+void pc_config(dungeon_t *d);
+uint32_t pc_next_pos(dungeon_t *d, pair_t dir);
+void place_pc(dungeon_t *d);
+void pc_learn_terrain(pc *p, pair_t pos, terrain_type_t ter);
+terrain_type_t pc_learned_terrain(pc *p, int8_t y, int8_t x);
+void pc_init_known_terrain(pc *p);
+void pc_observe_terrain(pc *p, dungeon_t *d);
+int32_t is_illuminated(pc *p, int8_t y, int8_t x);
+void pc_reset_visibility(pc *p);
+void pc_see_object(character *the_pc, object *o);
+
+#endif
